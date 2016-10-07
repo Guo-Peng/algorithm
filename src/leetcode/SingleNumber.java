eb005ffe45eef5f230e585edd9531fb7bb6d483c
Peterkwok <532957426@qq.com>
2016-10-07 16:57:02 +0800

    SumOfTwoIntegers

--- src/leetcode/SingleNumber.java
+++ src/leetcode/SingleNumber.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,21 +32,33 @@ public class SingleNumber {
         return pre;
     }
 
+    /**
+     * solution 2  2ms   best
+     * 异或操作
+     */
+    public static int singleNumber2(int[] nums) {
+        int c = 0;
+        for (int num : nums) {
+            c ^= num;
+        }
+        return c;
+    }
+
+
+    /**
+     * solution 3  17ms
+     * 依次添加至set中,若包含则删除
+     */
     public static int singleNumber(int[] nums) {
-        HashSet<String> s = new HashSet<>();
-        return 1;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.remove(num)) set.add(num);
+        }
+        return set.iterator().next();
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 2, 1, 3, 3, 4, 4, 6, 6, 8, 8, 9};
-//        System.out.println(singleNumber(arr));
-
-        String s = ",1,1,2,3,3,4,4,5,5";
-        Pattern p = Pattern.compile("(\\d+)(?!.*\\1)");
-        Matcher m = p.matcher(s);
-
-        while (m.find()) {
-            System.out.println(m.group(1));
-        }
+        int[] arr = {2, 2, 1, 3, 3, 4, 4, 6, 6, 8, 8};
+        System.out.println(singleNumber(arr));
     }
 }
---
+++ src/leetcode/SumOfTwoIntegers.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * 371
+ * Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+ * <p>
+ * Example:
+ * Given a = 1 and b = 2, return 3.
+ */
+
+public class SumOfTwoIntegers {
+    public static int getSum(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
