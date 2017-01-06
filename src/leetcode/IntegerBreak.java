ab4f49208d82ff1b504feaa163728d1ea2729bc2
Peterkwok <532957426@qq.com>
2017-01-07 01:04:44 +0800

    solution 2 3

--- src/leetcode/IntegerBreak.java
+++ src/leetcode/IntegerBreak.java
@@ -9,7 +9,10 @@ package leetcode;
  * Note: You may assume that n is not less than 2 and not larger than 58.
  */
 public class IntegerBreak {
+
     /**
+     * 分解后的数为均值时乘积最大 x^(n/x) 求导得到最值点为e 尽量拆为2 或者3必有最大值
+     * <p>
      * solution 1 16ms
      * 基于 2 3进行拆解 4可以拆为2 2 5可以拆为 2 3
      * 递归过程中始终保持拆解得到的积最大
@@ -19,11 +22,42 @@ public class IntegerBreak {
         else return Math.max(2 * divideInteger(n - 2), 3 * divideInteger(n - 3));
     }
 
-    public static int integerBreak(int n) {
+    public static int integerBreak1(int n) {
         if (n > 3)
             return divideInteger(n);
         else if (n == 3) return 2;
         else return 1;
+    }
+
+    /**
+     * solution 2   3ms
+     * 每一位依次拆解 并保存拆解最大值
+     */
+    public int integerBreak2(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], (Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+            }
+        }
+        return dp[n];
+    }
+
+    /**
+     * solution 3  1ms
+     * 修改最优解
+     * 可以只拆解 2 3
+     */
+    public static int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 2; j < i && j < 4; j++) {
+                dp[i] = Math.max(dp[i], (Math.max(j, dp[j])) * (Math.max(i - j, dp[i - j])));
+            }
+        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
