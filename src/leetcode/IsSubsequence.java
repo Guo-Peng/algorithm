77365f07f1796b38c96075fd9b19bc2b591eff4a
Peterkwok <532957426@qq.com>
2017-01-08 11:42:07 +0800

    binary search

--- src/leetcode/IsSubsequence.java
+++ src/leetcode/IsSubsequence.java
@@ -1,7 +1,6 @@
 package leetcode;
 
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * 392. Is Subsequence.
@@ -30,7 +29,7 @@ public class IsSubsequence {
      * solution 1    39ms
      * 只要出现即可,不需要检测多个子序列
      */
-    public static boolean isSubsequence(String s, String t) {
+    public static boolean isSubsequence1(String s, String t) {
         if (s.length() == 0) return true;
         int index = 0;
         for (int i = 0; i < t.length(); i++) {
@@ -40,8 +39,30 @@ public class IsSubsequence {
         return false;
     }
 
+    /**
+     * binary search  61ms
+     */
+    public boolean isSubsequence(String s, String t) {
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
+        }
+
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // 不含有该字符
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;  //不存在时返回-insertion - 1,insertion为该元素在List中的插入位置,如果insertion等于list长度则元素顺序不正确,不符合要求
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        String s = "axc";
+        String s = "acb";
         String t = "ahbgdc";
         System.out.println(isSubsequence(s, t));
     }
